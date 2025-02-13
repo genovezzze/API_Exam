1112cd3bb7e95c3e5cd7a62c2dc3933f63f31fb8
genovezzze <timongo555@gmail.com>
2025-02-13 09:43:04 +0200

    1.2

--- exam1.py
+++ exam1.py
@@ -1,11 +1,27 @@
 import csv
 csvarray = []
-with open("agenti.csv", newline='', encoding="utf-8") as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=",", quotechar="|")
+with open ('agenti.csv', newline="", encoding='utf-8') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=';', quotechar='|')
+    #print(spamreader)
     for row in spamreader:
         csvarray.append(row)
-    for rinda in csvarray:
-        print(rinda[0])
-print(csvarray)
-print(len(csvarray[0]))
-print(csvarray[1])
+    #print(csvarray)
+   # for rinda in csvarray:
+   #     print(rinda[0])
+#print(csvarray)
+#print(len(csvarray[0]))
+#print(csvarray[1][1])
+
+def filtretIestades(array):
+    for rinda in masivs:
+        if rinda[0]=="Valsts iestādes" or rinda[0]=="Izglitības iestāde":
+            print(rinda)
+            print(f"{rinda[0]}, {rinda[2]}")
+#filtretIestades(csvarray)
+
+def tikaiRiga(masivs):
+    for rinda in masivs:
+        if "Riga," in rinda[2]:
+            print(rinda[2])
+
+tikaiRiga(csvarray)
