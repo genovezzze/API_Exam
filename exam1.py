17c8a98d8c202ff49f86ccf0c3aca21cf2086e45
genovezzze <timongo555@gmail.com>
2025-02-13 09:34:39 +0200

    1.1

--- exam1.py
+++ exam1.py
@@ -1,1 +1,11 @@
-sad
+import csv
+csvarray = []
+with open("agenti.csv", newline='', encoding="utf-8") as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=",", quotechar="|")
+    for row in spamreader:
+        csvarray.append(row)
+    for rinda in csvarray:
+        print(rinda[0])
+print(csvarray)
+print(len(csvarray[0]))
+print(csvarray[1])
